a8608e44b088cf8bd042008abe1ec451fb8d9f4e
Brandon Waskiewicz <brandon.waskiewicz@gmail.com>
2011-02-11 16:13:56 -0500

    Finished hash table and karma implementation. Could still use refactoring but ehhhh.

--- src/main.c
+++ src/main.c
@@ -13,7 +13,8 @@
 #define   MAX_NICK_LENGTH 50
 #define   MAX_NICKS       1000
 
-struct irc_message {
+struct irc_message 
+{
   char prefix[IRC_BUF_LENGTH];
   char command[IRC_BUF_LENGTH];
   char params[IRC_BUF_LENGTH];
@@ -27,8 +28,7 @@ int send_msg(struct irc_message *message);
 int recv_msg(struct irc_message **message);
 struct irc_message *create_message(char *prefix, 
                                    char *command, 
-                                   int nparams,
-                                   ...);
+                                   char *params);
 void free_message(struct irc_message *message);
 int send_nick_user();
 struct irc_message *create_response(struct irc_message *msg);
@@ -39,14 +39,17 @@ int sockfd;
 GHashTable *karma_hash;
 int keep_alive;
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[]) 
+{
 
   int err, opt, aflag, cflag, nflag;
   err = opt = aflag = cflag = nflag = 0;
   keep_alive = 1;
 
-  while ((opt = getopt(argc, argv, "a:c:n:")) != -1) {
-    switch (opt) {
+  while ((opt = getopt(argc, argv, "a:c:n:")) != -1) 
+  {
+    switch (opt) 
+    {
       case 'a':
         address = optarg;
         aflag = 1;
@@ -65,7 +68,8 @@ int main(int argc, char *argv[]) {
     }
   }
 
-  if (err || !aflag || !cflag || !nflag) {
+  if (err || !aflag || !cflag || !nflag) 
+  {
     fprintf(stderr, "Usage: %s -a address[:port] -c channel -n nick\n",
             argv[0]);
     fprintf(stderr, "Make sure to quote the address and channel, due to :\n");
@@ -84,21 +88,26 @@ int main(int argc, char *argv[]) {
   struct irc_message *out_msg;
 
   bytes_rcved = recv_msg(&inc_msg);
-  do {
-    if (strcmp(inc_msg->command, "PING") == 0) {
-      out_msg = create_message(NULL, "PONG", 1, inc_msg->params);
+  do 
+  {
+    if (strcmp(inc_msg->command, "PING") == 0) 
+    {
+      out_msg = create_message(NULL, "PONG", inc_msg->params);
       send_msg(out_msg);
       free_message(out_msg);
 
-      if (!has_joined) {
-        out_msg = create_message(NULL, "JOIN", 1, channel);
+      if (!has_joined) 
+      {
+        out_msg = create_message(NULL, "JOIN", channel);
         send_msg(out_msg);
         free_message(out_msg);
         has_joined = 1;
       }
-    } else {
+    } else 
+    {
       out_msg = create_response(inc_msg);
-      if (out_msg != NULL) {
+      if (out_msg != NULL) 
+      {
         send_msg(out_msg);
         free_message(out_msg);
       }
@@ -107,7 +116,8 @@ int main(int argc, char *argv[]) {
     bytes_rcved = recv_msg(&inc_msg);
   } while (keep_alive && bytes_rcved > 0 && inc_msg != NULL);
 
-  if (inc_msg != NULL) {
+  if (inc_msg != NULL) 
+  {
     free_message(inc_msg);
   }
 
@@ -115,14 +125,16 @@ int main(int argc, char *argv[]) {
   save_karma();
 }
 
-int load_karma() {
+int load_karma() 
+{
   karma_hash = g_hash_table_new(g_str_hash, g_str_equal);
   FILE *fp = fopen("karma.txt", "r");
   char nick[MAX_NICK_LENGTH];
   int karma;
   char *n;
   int *k;
-  while (fscanf(fp, "%s\t%d\n", nick, &karma) != EOF) {
+  while (fscanf(fp, "%s\t%d\n", nick, &karma) != EOF) 
+  {
     n = (char*)malloc(strlen(nick)+1);
     k = (int*)malloc(sizeof(int));
     strcpy(n, nick);
@@ -132,13 +144,15 @@ int load_karma() {
   fclose(fp);
 }
 
-int save_karma() {
+int save_karma() 
+{
   GList *key_list = g_hash_table_get_keys(karma_hash);
   GList *keys = key_list;
   FILE *fp = fopen("karma.txt", "w");
   char *nick;
   int *karma;
-  while (keys != NULL) {
+  while (keys != NULL) 
+  {
     nick = (char*)keys->data;
     karma = (int*)g_hash_table_lookup(karma_hash, (gconstpointer)nick);
     fprintf(fp, "%s\t%d\n", nick, *karma);
@@ -152,62 +166,102 @@ int save_karma() {
   fclose(fp);
 }
 
-struct irc_message *create_response(struct irc_message *msg) {
+struct irc_message *create_response(struct irc_message *msg) 
+{
   struct irc_message *response = NULL;
-  /*if (strcmp(msg->command, "JOIN") == 0) {
-    char *nickname = strtok(msg->prefix, "!");
-    char *channel = strtok(msg->params, " ")+1;
-    if (strcmp(nickname+1, nick) != 0)
-      response = create_message(NULL, 
-                                "PRIVMSG", 
-                                3, 
-                                channel, 
-                                ":Hi", 
-                                nickname+1);
-
-  } else*/ if (strcmp(msg->command, "PRIVMSG") == 0) {
+  if (strcmp(msg->command, "PRIVMSG") == 0) 
+  {
     char *nickname = strtok(msg->prefix, "!")+1;
-    char *channel = strtok(msg->params, " ")+1; 
+    char *channel = strtok(msg->params, " "); 
     char *message = strtok(NULL, "")+1;
-    if (strcmp(nickname, "brandonw") == 0 &&
-        strcmp(message, "@QUIT") == 0) {
+    char buf[IRC_BUF_LENGTH];
+    char *tok = strtok(message, " ");
+    if (strcmp(tok, "!QUIT") == 0 && strcmp(nickname, "brandonw") == 0) 
+    {
       keep_alive = 0;
-      response = create_message(NULL, "QUIT", 0);
-      int *t = (int*)g_hash_table_lookup(karma_hash, "brandonw");
-      (*t)++;
+      response = create_message(NULL, "QUIT", NULL);
+    } else if (strcmp(tok, "!karma") == 0) 
+    {
+      tok = strtok(NULL, " ");
+      int* karma = (int*)g_hash_table_lookup(karma_hash, tok);
+      int k = 0;
+      if (karma != NULL) 
+      {
+        k = *karma;
+      }
 
+      sprintf(buf, "%s :%s has %d karma", channel, tok, k);
+      response = create_message(NULL, "PRIVMSG", buf);
+    } else if (strcmp(tok, "!up") == 0) 
+    {
+      tok = strtok(NULL, " ");
+      int* karma = (int*)g_hash_table_lookup(karma_hash, tok);
+      if (karma == NULL) 
+      {
+        karma = malloc(sizeof(int));
+        *karma = 0;
+        nick = malloc(strlen(tok)+1);
+        strcpy(nick, tok);
+        g_hash_table_insert(karma_hash, nick, karma);
+      }
+      (*karma)++;
+
+      sprintf(buf, "%s :%s has been upvoted to %d karma",
+              channel, tok, *karma);
+      response = create_message(NULL, "PRIVMSG", buf);
+    } else if (strcmp(tok, "!down") == 0) 
+    {
+      tok = strtok(NULL, " ");
+      int* karma = (int*)g_hash_table_lookup(karma_hash, tok);
+      if (karma == NULL) 
+      {
+        karma = malloc(sizeof(int));
+        *karma = 0;
+        nick = malloc(strlen(tok)+1);
+        strcpy(nick, tok);
+        g_hash_table_insert(karma_hash, nick, karma);
+      }
+      (*karma)--;
+
+      sprintf(buf, "%s :%s has been downvoted to %d karma",
+              channel, tok, *karma);
+      response = create_message(NULL, "PRIVMSG", buf);
     }
-
   }
   return response;
 }
 
-int send_nick_user() {
-  struct irc_message *nick_msg = create_message(NULL, "NICK", 1, nick);
+int send_nick_user() 
+{
+  struct irc_message *nick_msg = create_message(NULL, "NICK", nick);
   send_msg(nick_msg);
   free_message(nick_msg);
 
-  struct irc_message *user_msg = create_message(NULL, "USER", 6, nick, nick,
-                                                "8", "*", ":", nick);
+  char buf[IRC_BUF_LENGTH];
+  sprintf(buf, "USER %s %s 8 * : %s", nick, nick, nick);
+  struct irc_message *user_msg = create_message(NULL, "USER", buf);
   send_msg(user_msg);
   free_message(user_msg);
 
   return 0;
 }
 
-int connect_to_server(char *address) {
+int connect_to_server(char *address) 
+{
   struct addrinfo *addr;
   getaddr(&addr, address);
 
   int sockfd = socket(addr->ai_family, addr->ai_socktype, addr->ai_protocol);
-  if (sockfd < 0) {
+  if (sockfd < 0) 
+  {
     perror("socket");
     exit (EXIT_FAILURE);
   }
 
   int conn_result;
   conn_result = connect(sockfd, addr->ai_addr, addr->ai_addrlen);
-  if (conn_result < 0) {
+  if (conn_result < 0) 
+  {
     printf("Failure connecting to %s", address);
     return -1;
   }
@@ -216,12 +270,14 @@ int connect_to_server(char *address) {
   return sockfd;
 }
 
-int send_msg(struct irc_message *message) {
+int send_msg(struct irc_message *message) 
+{
   static char buf[IRC_BUF_LENGTH];
   memset(buf, 0, sizeof(buf));
   int idx = 0;
 
-  if (message->prefix[0] != '\0') {
+  if (message->prefix[0] != '\0') 
+  {
     sprintf(buf+idx, "%s ", message->prefix);
     idx += strlen(message->prefix) + 1;
   }
@@ -229,7 +285,8 @@ int send_msg(struct irc_message *message) {
   sprintf(buf+idx, "%s", message->command);
   idx += strlen(message->command);
 
-  if (message->params[0] != '\0') {
+  if (message->params[0] != '\0') 
+  {
     sprintf(buf+idx, " %s", message->params);
     idx += strlen(message->params) + 1;
   }
@@ -241,7 +298,8 @@ int send_msg(struct irc_message *message) {
   return send(sockfd, (void*)buf, strlen(buf), 0);
 }
 
-int recv_msg(struct irc_message **message) {
+int recv_msg(struct irc_message **message) 
+{
   static char buf[IRC_BUF_LENGTH];
   int bytes_rcved = 0;
   memset(buf, 0, sizeof(buf));
@@ -259,36 +317,40 @@ int recv_msg(struct irc_message **message) {
   prefix = command = params = NULL;
 
   char *tok = strtok(buf, " ");
-  if (tok[0] != ':') {
+  if (tok[0] != ':') 
+  {
     command = tok;
   }
-  else {
+  else 
+  {
     prefix = tok;
     command = strtok(NULL, " ");
   }
 
-  if ((tok = strtok(NULL, "\r\n")) != NULL) {
+  if ((tok = strtok(NULL, "\r\n")) != NULL) 
+  {
     params = tok;
   }
   
-  *message = create_message(prefix, command, 1, params);
+  *message = create_message(prefix, command, params);
   return bytes_rcved;
 }
 
 struct irc_message *create_message(char *prefix, 
                                    char *command, 
-                                   int nparams,
-                                   ...) {
-
+                                   char *params) 
+{
   struct irc_message *msg = malloc(sizeof(struct irc_message));
   memset(msg->prefix, 0, sizeof(msg->prefix));
   memset(msg->command, 0, sizeof(msg->command));
   memset(msg->params, 0, sizeof(msg->params));
 
   int len = 0;
-  if (prefix != NULL) {
+  if (prefix != NULL) 
+  {
     len += strlen(prefix) + 1; /* +1 for space after prefix */
-    if (len > IRC_BUF_LENGTH) {
+    if (len > IRC_BUF_LENGTH) 
+    {
       free_message(msg);
       return NULL;
     }
@@ -296,48 +358,35 @@ struct irc_message *create_message(char *prefix,
   }
 
   len += strlen(command);
-  if (len > IRC_BUF_LENGTH) {
+  if (len > IRC_BUF_LENGTH) 
+  {
     free_message(msg);
     return NULL;
   }
   strcpy(msg->command, command);
 
-  if (nparams > 0) {
-    va_list ap;
-    va_start(ap, nparams);
-    char *param;
-    int param_idx = 0;
-    int is_first_param = 1;
-
-    while (nparams > 0) {
-      param = va_arg(ap, char *);
-      len += strlen(param) + 1;
-      if (len > IRC_BUF_LENGTH) {
-        free_message(msg);
-        va_end(ap);
-        return NULL;
-      }
-      if (!is_first_param) {
-        strcpy(msg->params + param_idx, " ");
-        param_idx++;
-      }
-      strcpy(msg->params + param_idx, param);
-      param_idx += strlen(param);
-      nparams--;
-      is_first_param = 0;
+  if (params != NULL) 
+  {
+    len += strlen(params) + 1; /* +1 for space before params */
+    if (len > IRC_BUF_LENGTH) 
+    {
+      free_message(msg);
+      return NULL;
     }
-    va_end(ap);
+    strcpy(msg->params, params);
   }
 
   return msg;
 }
 
-void free_message(struct irc_message *message) {
+void free_message(struct irc_message *message) 
+{
   free(message);
   return;
 }
 
-int getaddr (struct addrinfo **result, char *address) {
+int getaddr (struct addrinfo **result, char *address) 
+{
   char addr_cpy[strlen(address)];
   strcpy(addr_cpy, address);
 
@@ -358,7 +407,8 @@ int getaddr (struct addrinfo **result, char *address) {
                   port == NULL ? DEFAULT_PORT : port, 
                   &hints, 
                   result);
-  if (s != 0) {
+  if (s != 0) 
+  {
     printf("error getting addrinfo");
     exit(EXIT_FAILURE);
   }
