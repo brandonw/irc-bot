ffa2403b2986cc40273b723929c68fffca424a69
Peter Johnson <zidsoft@gmail.com>
2011-03-09 17:59:42 +0000

    Tidy up quit.c
    Fixed code and declaration mixture, and marked command as static.

--- src/quit.c
+++ src/quit.c
@@ -2,17 +2,18 @@
 #include <string.h>
 #include "bot.h"
 
-const char command[] = "PRIVMSG";
+static const char command[] = "PRIVMSG";
 
 int create_response(struct irc_message *msg, struct irc_message **messages,
 		    int *msg_count)
 {
-	*msg_count = 0;
 	char *msg_nick = strtok(msg->prefix, "!") + 1;
-	char *msg_channel = strtok(msg->params, " ");
 	char *msg_message = strtok(NULL, "") + 1;
-	char buf[IRC_BUF_LENGTH];
-	char *tok = strtok(msg_message, " ");
+	char *tok;
+
+	tok  = strtok(msg_message, " ");
+	*msg_count = 0;
+
 	if (strcmp(tok, "!QUIT") == 0 && strcmp(msg_nick, "brandonw") == 0) {
 		kill_bot();
 		messages[0] = create_message(NULL, "QUIT", NULL);
