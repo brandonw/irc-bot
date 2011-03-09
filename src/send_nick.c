391edb91d49b30d42d8d5f434f4408d4343e895f
Peter Johnson <zidsoft@gmail.com>
2011-03-09 17:26:09 +0000

    send_nick.c rewritten
    Removed the extra write to msg_count, fixed the mixed code and
    declarations, un-nested some code by inverting an if statement.

--- src/send_nick.c
+++ src/send_nick.c
@@ -9,15 +9,15 @@ const char command[] = "NOTICE";
 int create_response(struct irc_message *msg, struct irc_message **messages,
 		    int *msg_count)
 {
-	*msg_count = 0;
+	char buf[IRC_BUF_LENGTH];
 
-	if (!has_sent_nick) {
-		messages[0] = create_message(NULL, "NICK", nick);
-		char buf[IRC_BUF_LENGTH];
-		sprintf(buf, "USER %s %s 8 * : %s", nick, nick, nick);
-		messages[1] = create_message(NULL, "USER", buf);
-		*msg_count = 2;
-	}
+	if (has_sent_nick)
+		return 0;
+
+	messages[0] = create_message(NULL, "NICK", nick);
+	sprintf(buf, "USER %s %s 8 * : %s", nick, nick, nick);
+	messages[1] = create_message(NULL, "USER", buf);
+	*msg_count = 2;
 
 	return 0;
 }
