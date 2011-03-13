f3b39aa12101dd3dd1c68d5fcc22496aa4be84e2
Brandon Waskiewicz <brandon.waskiewicz@gmail.com>
2011-03-12 23:41:30 -0500

    Fixed string tokenizing in quit.c
    Fixed bugs preventing quit plugin from recognizing quit command due to
    tokenizing bugs.
    
    Reformatted code so declarations are at start of the block.
    
    Fixed bug where command was marked as static, thus preventing plugin
    loader from seeing it as a valid plugin.

--- src/quit.c
+++ src/quit.c
@@ -2,19 +2,20 @@
 #include <string.h>
 #include "bot.h"
 
-static const char command[] = "PRIVMSG";
+#include <stdio.h>
+const char command[] = "PRIVMSG";
 
 int create_response(struct irc_message *msg, struct irc_message **messages,
 		    int *msg_count)
 {
-	char *msg_nick = strtok(msg->prefix, "!") + 1;
-	char *msg_message = strtok(NULL, "") + 1;
-	char *tok;
+	char *msg_nick, *msg_message;
+	msg_nick = strtok(msg->prefix, "!") + 1;
+	msg_message = strtok(msg->params, " ");
+	msg_message = strtok(NULL, "") + 1;
 
-	tok  = strtok(msg_message, " ");
 	*msg_count = 0;
-
-	if (strcmp(tok, "!QUIT") == 0 && strcmp(msg_nick, "brandonw") == 0) {
+	
+	if (strcmp(msg_message, "!QUIT") == 0 && strcmp(msg_nick, "brandonw") == 0) {
 		kill_bot();
 		messages[0] = create_message(NULL, "QUIT", NULL);
 		*msg_count = 1;
