f505cbe91f7da9aa3afed0c93d5862684dc3aa2d
Jason Dana <jason.dana@gmail.com>
2011-04-26 22:00:59 -0400

    Add SIGTERM handling.

--- src/main.c
+++ src/main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <signal.h>
 #include "bot.h"
 
 char *address = NULL;
@@ -42,6 +43,8 @@ int main(int argc, char *argv[])
 		fprintf(stderr, "being special characters.\n");
 		exit(EXIT_FAILURE);
 	}
+
+    signal(SIGTERM, kill_bot);
 
 	run_bot();
 
